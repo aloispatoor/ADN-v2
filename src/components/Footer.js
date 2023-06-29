47ff0e42040f312321c5793fa69591d2269a80c5
Alois Patoor <palois89@posteo.net>
2023-06-29 15:58:22 +0200

    Fontawesome added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import '../styles/app.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 function Footer(){
     return (
         <footer>
@@ -8,15 +9,21 @@ function Footer(){
                     <p className="walkwayFont">ADN</p>
                     <p className="walkwayFont">Au-delà des Normes</p>
                     <div className="socialNetworks">
-                        <a href="https://www.facebook.com/AuDeladesNormes" target="_blank"><i
-                            className="fab fa-facebook"></i></a>
-                        <a href="https://twitter.com/AuDN24" target="_blank"><i
-                            className="fab fa-twitter-square"></i></a>
-                        <a href="https://instagram.com/au.dela.des.normes.24" target="_blank"><i
-                            className="fab fa-instagram"></i></a>
-                        <a href="https://www.youtube.com/channel/UCP22Pg2BFAXY3jVZs7fpM0Q" target="_blank"><i
-                            className="fa-brands fa-youtube"></i></a>
-                        <a href="mailto:contact@audeladesnormes.org"><i className="fas fa-envelope"></i></a>
+                        <a href="https://www.facebook.com/AuDeladesNormes" target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
+                        </a>
+                        <a href="https://twitter.com/AuDN24" target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-twitter" />
+                        </a>
+                        <a href="https://instagram.com/au.dela.des.normes.24" target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-instagram" />
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCP22Pg2BFAXY3jVZs7fpM0Q" target="_blank">
+                            <FontAwesomeIcon icon="fa-brands fa-square-youtube" />
+                        </a>
+                        <a href="mailto:contact@audeladesnormes.org">
+                            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                        </a>
                     </div>
                 </div>
                 <div className="footerSections">
